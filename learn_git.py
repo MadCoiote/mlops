7c4af5d20621d97f57acd36a4100f5e037b474b2
MadCoiote <hernandesfagundes@gmail.com>
2023-11-10 05:29:06 -0300

    matriz criada

---
+++ learn_git.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 10 05:24:56 2023
+
+@author: herna
+"""
+import numpy as np
+
+
+"Step 1"
+A = np.array([[1,2,3],[3,2,1],[0,5,0]])
+print("Matriz de avaliação: \n{}".format(A))
