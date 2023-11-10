a210bd94a642f17430410053ea21af53de54c707
MadCoiote <hernandesfagundes@gmail.com>
2023-11-10 05:54:19 -0300

    identity introduced

--- learn_git.py
+++ learn_git.py
@@ -7,6 +7,12 @@ Created on Fri Nov 10 05:24:56 2023
 import numpy as np
 
 
-"Step 1"
+"Step 1: Create intendend matrix"
 A = np.array([[1,2,3],[3,2,1],[0,5,0]])
 print("Matriz de avaliação: \n{}".format(A))
+
+"Create the identity matrix"
+I = np.identity(A.shape[0])
+
+"Compute the determinant"
+
