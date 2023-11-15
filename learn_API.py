6c9a0e0e3bde5bab82c0bc3082428eefd3ec3749
MadCoiote <hernandesfagundes@gmail.com>
2023-11-15 19:52:19 -0300

    updating code

--- learn_API.py
+++ learn_API.py
@@ -7,8 +7,23 @@ Created on Wed Nov 15 06:55:38 2023
 
 from fastapi import FastAPI
 
-app = FastAPI()
-@app.get(path="/")
+app = FastAPI(title="API de Hernandes",                         #Cria uma instância do objeto API
+              openapi_tags=[                                    # openapi_tags recebe uma lista de dicionários
+                  {"name": "Health",
+                      "description":"Get API Health"},
+                  {"name": "Prediction",
+                      "description":"Model Prediction"}])
 
-def home():
+def load_model():
+    #configureMLFLOW
+    return 0
+
+@app.get(path="/",                                              #@ é um decorator que envelopa a função, path é a rota e a barra significa raiz
+         tags=["Health"])                                       #inclui o método get no tag Health definido na criação da API      
+def api_health():
     return {"status":"healthy"}
+
+@app.post(path="/predict",
+          tags=["Prediction"])
+def predict():
+    return {"prediction": 0}
